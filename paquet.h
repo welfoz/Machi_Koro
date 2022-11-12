bccc8617a2bb1cc9b4c0566ee8f0fb49e8d1b352
felixbrossard79 <felix.brossard@etu.utc.fr>
2022-11-12 12:08:06 +0100

    Oui

--- paquet.h
+++ paquet.h
@@ -1,11 +1,6 @@
-//
-// Created by Félix Brossard on 12/11/2022.
-//
-
 #ifndef MACHI_KORO_PAQUET_H
 #define MACHI_KORO_PAQUET_H
 
 
 
-// Coucou je teste git. Est ce que ça marche ?
 #endif //MACHI_KORO_PAQUET_H
