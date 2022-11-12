a0b8be297ace8b4c486538986fa179cb713c19ee
Pol Corty <pol.corty@etu.utc.fr>
2022-11-12 20:16:49 +0100

    initial commit

---
+++ LO21 - Machi Koro/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LO21 - Machi Koro
+//
+//  Created by Pol Corty on 12/11/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
