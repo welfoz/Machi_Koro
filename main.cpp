b31c3a278bc55582eec41aec861426adacdbf91f
Pol Corty <pol.corty@etu.utc.fr>
2022-11-12 21:07:30 +0100

    test pol

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LO21 - Machi Koro
+//
+//  Created by Pol Corty on 12/11/2022.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
