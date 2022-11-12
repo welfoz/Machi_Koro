d5fbf642698a42e88e492a2e7d3d30ccb447427b
jules <jules.taverne@etu.utc.fr>
2022-11-12 14:38:30 +0100

    joueur.h banque.h et joueur.h

---
+++ banque.h
@@ -0,0 +1,17 @@
+#ifndef MACHI_KORO_BANQUE_H
+#define MACHI_KORO_BANQUE_H
+#include "compte.h"
+#include "joueur.h"
+class Banque {
+private :
+    Banque(**Joueur);
+    ~Banque();
+    Compte* comptes;
+    friend class Jeu;
+    void echange(class Joueur& j1, class Joueur& j2, int montant);
+    void credit (class Joueur& j1, class Joueur& j2, int montant);
+    void prelevement(class Joueur& j, int montant);
+
+};
+
+#endif //MACHI_KORO_BANQUE_H
