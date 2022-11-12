49bdd2146d25fe95af058e1b7b060685d1d8ff5d
Xavier Lemerle <xavier.lemerle@etu.utc.fr>
2022-11-12 15:34:47 +0100

    carte.h

--- carte.h
+++ carte.h
@@ -5,8 +5,14 @@
 #ifndef MACHI_KORO_CARTE_H
 #define MACHI_KORO_CARTE_H
 
-class Carte{
-
+class Carte {
+private:
+	string name;
+	string type; 
+	int* activation_numeros;
+	int number_of_numeros;
+	int prix;
+	Icon icon;
 };
 
 #endif //MACHI_KORO_CARTE_H
