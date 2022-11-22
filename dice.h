527ddf2a9c8fa6d64004de6bdd80dc067151648e
Fabien MICHEL <fabien@michel.com>
2022-11-22 20:04:06 +0100

    de -> dice

---
+++ dice.h
@@ -0,0 +1,17 @@
+//
+// Created by jules on 12/11/2022.
+//
+
+#ifndef MACHI_KORO_DE_H
+#define MACHI_KORO_DE_H
+
+class Dice {
+    friend class Game;
+    static const int min = 1;
+    static const int max = 6;
+    const size_t throwDice() const{
+        return 0;
+    };
+};
+
+#endif //MACHI_KORO_DE_H
