f4b96900433e00b525cf5fe5d16ff337ffb88f5d
Pol Corty <pol.corty@etu.utc.fr>
2022-11-28 08:38:07 +0100

    maj pol

--- bank.h
+++ bank.h
@@ -7,7 +7,7 @@ private :
     Account** accounts;
     friend class Game;
     size_t nbPlayers;
-    Bank(size_t nbPlayers) : accounts(new Account*[nbPlayers]){};
+    Bank(size_t nb) : accounts(new Account*[nb]), nbPlayers(nb){};
     ~Bank(){ for (size_t i=0;i<nbPlayers;i++) delete accounts[i];}
     void trade(size_t idReceiver, size_t idGiver, int amount);
     void credit(size_t idReceiver, int amount);
