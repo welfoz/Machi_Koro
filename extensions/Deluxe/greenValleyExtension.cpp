7c566ef753917780951b6abc06823711c23d8f01
jules <jules.taverne@etu.utc.fr>
2022-12-05 17:07:57 +0100

    fix idCurrentPlayer

---
+++ extensions/Deluxe/greenValleyExtension.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jules on 05/12/2022.
+//
+
+#include "greenValleyExtension.h"
---
+++ extensions/Deluxe/greenValleyExtension.h
@@ -0,0 +1,14 @@
+//
+// Created by jules on 05/12/2022.
+//
+
+#ifndef MACHI_KORO_GREENVALLEYEXTENSION_H
+#define MACHI_KORO_GREENVALLEYEXTENSION_H
+
+
+class greenValleyExtension {
+
+};
+
+
+#endif //MACHI_KORO_GREENVALLEYEXTENSION_H
