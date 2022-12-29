2006f9baa52236be693891c1552cb496a0a6787e
Fabien MICHEL <michelfabien30650@gmail.com>
2022-12-29 14:30:41 +0100

    fix gui/cli typing error

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 #include "interface/cli.h"
 
 // comment to play in cli
-//#include <QApplication>
-//#include "interface/gui.h"
+#include <QApplication>
+#include "interface/gui.h"
 
 namespace MACHI_KORO {
 
@@ -20,7 +20,7 @@ void play(Interface::Option option) {
         break;
     case Interface::Option::gui:
         // comment to play in cli
-        //interface = new Gui();
+        interface = new Gui();
         break;
     case Interface::Option::cliGreenValley:
         interface = new GreenValleyCli();
@@ -57,19 +57,19 @@ void play(Interface::Option option) {
 }
 
 // to play in CLI
-int main(int argc, char* argv[]) {
+//int main(int argc, char* argv[]) {
 
-    MACHI_KORO::play(Interface::Option::cli);
+//    MACHI_KORO::play(Interface::Option::cli);
 
-    return 0;
-}
+//    return 0;
+//}
 
 // to play in GUI
-//int main(int argc, char* argv[]) {
-//
-//    QApplication app(argc, argv);
-//
-//    MACHI_KORO::play(Interface::Option::cli);
-//
-//    return app.exec();
-//}
+int main(int argc, char* argv[]) {
+
+    QApplication app(argc, argv);
+
+    MACHI_KORO::play(Interface::Option::gui);
+
+    return app.exec();
+}
