1f2d743942dcc58f6b7e100ea6d50970c81278ea
jules <jules.taverne@etu.utc.fr>
2022-12-29 18:20:44 +0100

    include controller

--- interface/Ai.h
+++ interface/Ai.h
@@ -5,7 +5,7 @@
 #pragma once
 #include "./interface.h"
 #include "random"
-
+#include "../game/controller/control.h"
 class Ai : public Interface{
     template<typename t> t getAiChoice(std::vector<t> options,std::vector<t> exceptions={}) const;
     bool confirmationDialog(string message, string firstOption, string secondOption) override;
